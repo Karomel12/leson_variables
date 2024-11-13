4139e8d47b06726e0aa00379421367ad0299d2be
Karomel12 <denisboarsci@gmail.com>
2024-11-13 19:17:20 +0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+home_work = 12
+hours = 1.5
+course = 'Python'
+time_for_one_work = hours/home_work
+print ('Курс:', course, 'всего задач:', home_work, 'затраченно часов:', hours, 'среднее время выполнения:', time_for_one_work)
